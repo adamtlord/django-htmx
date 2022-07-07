0e8034cd4513c6144af4c90808bc658c902e0109
Adam Lord <adam@tivix.com>
2022-07-07 14:36:35 -0700

    simplify, user prefs dark mode default

--- django_backend/src/static/js/index.js
+++ django_backend/src/static/js/index.js
@@ -1,6 +1,14 @@
-import Alpine from 'alpinejs';
-import app from './modules/app';
+/* global Alpine */
 
-window.Alpine = Alpine;
-Alpine.data('app', app);
-Alpine.start();
+const App = () => ({
+  darkMode: localStorage.getItem('dark') === 'true' || (!('dark' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches),
+  toggleDarkMode() {
+    const currentVal = this.darkMode;
+    this.darkMode = !currentVal;
+    localStorage.setItem('dark', !currentVal);
+  },
+});
+
+document.addEventListener('alpine:init', () => {
+  Alpine.data('App', App);
+});
--- django_backend/src/static/js/modules/app.js
+++
@@ -1,8 +0,0 @@
-export default () => ({
-  darkMode: localStorage.getItem('dark') === 'true',
-  toggleDarkMode() {
-    const currentVal = this.darkMode;
-    this.darkMode = !currentVal;
-    localStorage.setItem('dark', !currentVal);
-  },
-});
